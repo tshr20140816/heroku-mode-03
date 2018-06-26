8817a27734227bf402700d844617f1bf71c73c83
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-06-26 15:25:11 +0900

    Update get_rss.php

--- www/get_rss.php
+++ www/get_rss.php
@@ -17,7 +17,6 @@ $item_description = $data[8];
 
 $items_template = '<item><title>__TITLE__</title><link>__LINK__</link><description>__DESCRIPTION__</description><pubDate/></item>';
 
-//$html = mb_convert_encoding(file_get_contents($url), 'UTF-8', $encoding);
 list($contents, $http_code) = get_contents($url);
 if ($http_code != '200') {
   loggly_log("ERROR : HTTP STATUS ${http_code} ${url}");
